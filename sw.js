5fc94376a8ab9526e361273dd631daa455e73f93
sfiocchi01-dev <sfiocchi01@gmail.com>
2026-09-12 15:42:07 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,3 @@
+const C='stickman-v1';const A=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
